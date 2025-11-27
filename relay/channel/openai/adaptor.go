f91c1d592fcee177f2e26e8debf63b1be5cace7c
KKKe <sue7game@163.com>
2025-11-27 11:48:55 +0800

    去掉打印信息

--- relay/channel/openai/adaptor.go
+++ relay/channel/openai/adaptor.go
@@ -585,7 +585,6 @@ func (a *Adaptor) DoRequest(c *gin.Context, info *relaycommon.RelayInfo, request
 }
 
 func (a *Adaptor) DoResponse(c *gin.Context, resp *http.Response, info *relaycommon.RelayInfo) (usage any, err *types.NewAPIError) {
-
 	switch info.RelayMode {
 	case relayconstant.RelayModeRealtime:
 		err, usage = OpenaiRealtimeHandler(c, info)
